0933924f74fac732daee2915c097bf5d4f358b91
David Greminger <david.greminger@1up.io>
2015-01-15 11:56:11 +0100

    Added lib

---
+++ src/Module/iCalExport.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Oneup\iCalExport\Module;
+
+use Eluceo\iCal\Component\Calendar;
+use Eluceo\iCal\Component\Event;
+
+class iCalExport extends \Events
+{
+    protected $strTemplate = 'mod_ical_export';
+
+    public function generate()
+    {
+        if (TL_MODE == 'BE')
+        {
+            $objTemplate = new \BackendTemplate('be_wildcard');
+
+            $objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['ical_export'][0]) . ' ###';
+            $objTemplate->title = $this->headline;
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+
+            return $objTemplate->parse();
+        }
+
+        // Set the item from the auto_item parameter
+        if (!isset($_GET['events']) && $GLOBALS['TL_CONFIG']['useAutoItem'] && isset($_GET['auto_item']))
+        {
+            \Input::setGet('events', \Input::get('auto_item'));
+        }
+
+        // Do not index or cache the page if no event has been specified
+        if (!\Input::get('events'))
+        {
+            global $objPage;
+            $objPage->noSearch = 1;
+            $objPage->cache = 0;
+            return '';
+        }
+
+        $this->cal_calendar = $this->sortOutProtected(deserialize($this->cal_calendar));
+
+        // Do not index or cache the page if there are no calendars
+        if (!is_array($this->cal_calendar) || empty($this->cal_calendar))
+        {
+            global $objPage;
+            $objPage->noSearch = 1;
+            $objPage->cache = 0;
+            return '';
+        }
+
+        return parent::generate();
+    }
+
+    public function sendIcsFile($objEvent)
+    {
+        $vCalendar = new Calendar(\Environment::get('url'));
+        $vEvent    = new Event();
+        $noTime    = false;
+
+        if ($objEvent->startTime === $objEvent->startDate && $objEvent->endTime === $objEvent->endDate) {
+            $noTime = true;
+        }
+
+        $vEvent
+            ->setDtStart(\DateTime::createFromFormat("d.m.Y - H:i:s", date("d.m.Y - H:i:s", $objEvent->startTime)))
+            ->setDtEnd(\DateTime::createFromFormat("d.m.Y - H:i:s", date("d.m.Y - H:i:s", $objEvent->endTime)))
+            ->setSummary($objEvent->title)
+            ->setUseUtc(false)
+            ->setLocation($objEvent->location)
+            ->setNoTime($noTime);
+        ;
+
+        $vCalendar->addComponent($vEvent);
+
+        header('Content-Type: text/calendar; charset=utf-8');
+        header('Content-Disposition: attachment; filename="' . $objEvent->alias . '.ics"');
+
+        echo $vCalendar->render();
+
+        exit;
+    }
+
+    protected function compile()
+    {
+        global $objPage;
+
+        $objEvent = \CalendarEventsModel::findPublishedByParentAndIdOrAlias(\Input::get('events'), $this->cal_calendar);
+
+        if (\Input::get('ics') === '') {
+            $this->sendIcsFile($objEvent);
+        }
+
+        $this->Template->href     = \Environment::get('request') . "?ics";
+        $this->Template->title    = "Im Kalendar eintragen";
+        $this->Template->link     = "Im Kalendar eintragen";
+        $this->Template->objEvent = $objEvent->startTime;
+    }
+}
