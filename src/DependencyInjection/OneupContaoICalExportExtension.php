ddb81776192fd6aaf0bd45609dfbb54e3c4c3819
David Greminger <david.greminger@1up.io>
2021-11-30 11:22:44 +0100

    Fix typo

--- src/DependencyInjection/OneupContaoICalExportExtension.php
+++ src/DependencyInjection/OneupContaoICalExportExtension.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Oneup\Contao\LanguageDependentModulesBundle\DependencyInjection;
+namespace Oneup\Contao\ICalExportBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class OneupContaoIcalExportExtension extends Extension
+class OneupContaoICalExportExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
